c3db2450043bec8a2ace735ec9e868076bced901
henry <mvasquez1987@hotmail.es>
2021-09-16 19:12:29 +0200

    se agrega una nueva carpeta y el archivo decoration

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/auth_background.dart';
 import 'package:productos_app/widgets/card_container.dart';
 
@@ -22,10 +23,9 @@ class LoginScreen extends StatelessWidget {
             ],
           )),
           SizedBox(height: 50),
-          Text(
-            'Crear una nueva cuenta',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          )
+          Text('Crear una nueva cuenta',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          SizedBox(height: 50),
         ],
       ),
     )));
@@ -43,19 +43,37 @@ class _LoginForm extends StatelessWidget {
             TextFormField(
               autocorrect: false,
               keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.deepPurple),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.deepPurple, width: 2)),
+              decoration: InputDecorations.authInputDecoration(
                   hintText: 'john.doe@gmail.com',
-                  labelText: 'Correo electrónico',
-                  labelStyle: TextStyle(color: Colors.grey),
-                  prefixIcon: Icon(Icons.alternate_email_sharp,
-                      color: Colors.deepPurple)),
-            )
+                  labelText: 'Correo electronico',
+                  prefixIcon: Icons.alternate_email_rounded),
+            ),
+            SizedBox(height: 30),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '*****',
+                  labelText: 'Contraeña',
+                  prefixIcon: Icons.lock_outline),
+            ),
+            SizedBox(height: 30),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.deepPurple,
+                child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                    child: Text(
+                      'Imgresar',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                onPressed: () {
+                  //TODO:Login Form
+                })
           ],
         ),
       ),
